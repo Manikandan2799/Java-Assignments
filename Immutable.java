76104fdccfade98369b4cbba64363ce7e05d1680
Manikandan2799 <112944858+Manikandan2799@users.noreply.github.com>
2022-11-02 10:07:19 +0530

    Create Immutable.java

---
+++ Immutable.java
@@ -0,0 +1,12 @@
+package JavaPractices;
+public class Immutable {
+        public static void main(String[] args)
+        {
+            String string1="Java is";
+            String string2="Immutable";
+            string1.concat (string2);
+            System.out.println("string is: "+string1);
+        }
+
+    }
+

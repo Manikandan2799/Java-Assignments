bbee3da69ccaaac14630b9165341f2be81aef3a3
Manikandan <112944858+Manikandan2799@users.noreply.github.com>
2022-11-24 17:11:37 +0530

    Create AntiClockWise.java

---
+++ 12.11.22/AntiClockWise.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AntiClockWise {
+        public static void main(String[] args) {
+            Scanner input=new Scanner(System.in);
+            int a[][]=new int[3][3];
+            for(int i=0;i<3;i++)
+                for (int j = 0; j < 3; j++) {
+                    a[i][j] = input.nextInt();
+                }
+            for(int i=2;i>=0;i--)
+            {
+                for(int j=0;j<3;j++)
+                {
+                    System.out.print(a[j][i]+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
